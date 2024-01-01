934a6498d44a19c12cd4bda841c99c56d48cc8a3
muazabdulr <muaz.abdulr@gmail.com>
2024-01-01 15:10:55 +0700

    Revert "draggable window, mobile viewport scaling"
    This reverts commit d2fb759061d4b96c511d88e2e07ceb10cc918f5b.

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,16 +1,10 @@
 import dynamic from 'next/dynamic'
-import { Metadata, Viewport } from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: 'Map | TRASK',
     description: 'TRASK adalah sistem informasi geografis yang memetakan data berkaitan dengan risiko kecelakaan',
 }
-
-const viewport: Viewport = {
-    width: "600",
-    interactiveWidget: "overlays-content"
-}
-export { viewport };
 
 const MapComp = dynamic(() => import('@/components/Map/MapComponent'), { ssr: false });
 
